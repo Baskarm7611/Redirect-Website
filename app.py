b0ea215c4b932ac6e363d958cc45bbd82835e729
Baskarm7611 <64820629+Baskarm7611@users.noreply.github.com>
2024-09-23 12:21:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
-from flask import Flask, redirect, request
+from quart import Quart, redirect, request
 import os
 import random
 from dotenv import load_dotenv
+
 load_dotenv()
 
-app = Flask(__name__)
+app = Quart(name)
 
 DL_WEBSITE_DOMAIN = os.environ.get("DL_WEBSITE_DOMAIN")
 FS_WEBSITE_DOMAIN = [x.strip() for x in os.environ.get("FS_WEBSITE_DOMAIN").split()]
 PORT = int(os.environ.get("PORT", 80))
 
 
-@app.route("/dl/")
-def home(path):
-    url = request.url.replace(f"{request.host}/dl", DL_WEBSITE_DOMAIN)
+@app.route("/dl/<path:path>")
+async def home(path):
+    url = request.url.replace(f"{request.host}/dl/{path}", DL_WEBSITE_DOMAIN)
     return redirect(url)
 
 
 @app.route("/fs/")
-def fs_home():
+async def fs_home():
     domain = random.choice(FS_WEBSITE_DOMAIN)
     url = request.url.replace(f"{request.host}/fs", domain)
     return redirect(url)
+    
+@app.route("/store/<path:path>")
+async def file_store(path):
+    url = f"https://telegram.me/THDT_FP3bot?start={path}"
+    return redirect(url)
 
 
-if __name__ == "__main__":
+if name == "main":
     app.run(host="0.0.0.0", port=PORT)
