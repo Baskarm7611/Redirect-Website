7685a9b4dac805d9e313222e2a557e16b43a8b95
Tamilgram <107699896+Tamilgram@users.noreply.github.com>
2024-09-24 17:54:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,24 @@
-from quart import Quart, redirect, request
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 import os
 import random
 from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Quart(name)
+app = FastAPI()
 
 DL_WEBSITE_DOMAIN = os.environ.get("DL_WEBSITE_DOMAIN")
 FS_WEBSITE_DOMAIN = [x.strip() for x in os.environ.get("FS_WEBSITE_DOMAIN").split()]
 PORT = int(os.environ.get("PORT", 80))
 
 
-@app.route("/dl/<path:path>")
-async def home(path):
-    url = request.url.replace(f"{request.host}/dl/{path}", DL_WEBSITE_DOMAIN)
-    return redirect(url)
-
-
-@app.route("/fs/")
-async def fs_home():
-    domain = random.choice(FS_WEBSITE_DOMAIN)
-    url = request.url.replace(f"{request.host}/fs", domain)
-    return redirect(url)
-    
-@app.route("/store/<path:path>")
-async def file_store(path):
+@app.get("/store/{path:path}")
+async def file_store(path: str):
     url = f"https://telegram.me/THDT_FP3bot?start={path}"
-    return redirect(url)
+    return RedirectResponse(url)
 
 
-if name == "main":
-    app.run(host="0.0.0.0", port=PORT)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
